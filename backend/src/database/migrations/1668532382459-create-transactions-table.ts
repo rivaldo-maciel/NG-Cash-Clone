fe8b7eebc96601a59bef05875dd9100e9d3004db
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-11-15 14:34:21 -0300

    feat: create transactions migration

--- backend/src/database/migrations/1668532382459-create-transactions-table.ts
+++ backend/src/database/migrations/1668532382459-create-transactions-table.ts
@@ -1,11 +1,46 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class createTransactionsTable1668532382459 implements MigrationInterface {
+export class createTransactionsTable1668532382459
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'transactions',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            isNullable: false
+          },
+          {
+            name: 'debited_account_id',
+            type: 'int',
+            isNullable: false
+          },
+          {
+            name: 'credited_account_id',
+            type: 'int',
+            isNullable: false
+          },
+          {
+            name: 'value',
+            type: 'decimal',
+            isNullable: false
+          },
+          {
+            name: 'created_at',
+            type: 'timestamps',
+            isGenerated: true
+          }
+        ]
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('transactions');
+  }
 }
