495a09194e6f873b768844e1e9ce14c6079feaa1
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-11-15 17:51:08 -0300

    feat: create Account model

---
+++ backend/src/database/models/Account.ts
@@ -0,0 +1,12 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+class Account {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'decimal', precision: 15, scale: 2, default: 0 })
+  balance: string;
+}
+
+export default Account;
