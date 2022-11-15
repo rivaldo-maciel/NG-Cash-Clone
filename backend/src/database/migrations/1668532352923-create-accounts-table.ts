164b3369ab8a93e5685242ac04865fd43566afd4
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-11-15 14:28:13 -0300

    feat: create accounts migration

--- backend/src/database/migrations/1668532352923-create-accounts-table.ts
+++ backend/src/database/migrations/1668532352923-create-accounts-table.ts
@@ -1,11 +1,29 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class createAccountsTable1668532352923 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'accounts',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            isNullable: false
+          },
+          {
+            name: 'balance',
+            type: 'decimal',
+            isNullable: false
+          },
+        ]
+      })
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('accounts');
+  }
 }
