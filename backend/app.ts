436f1bcf618e45351c27dd802fc681613d5b0fa0
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-11-16 11:09:34 -0300

    feat: create application class

--- backend/app.ts
+++ backend/app.ts
@@ -1,1 +1,36 @@
 import 'reflect-metadata';
+import * as express from 'express';
+import { Router, ErrorRequestHandler, RequestHandler } from 'express';
+
+class App {
+  private express: express.Application;
+
+  constructor() {
+    this.express = express();
+    this.config();
+  }
+
+  private config(): void {
+    this.express.use(express.json());
+  }
+
+  public routes(route: string, router: Router): void {
+    this.express.use(route, router);
+  }
+
+  public useErrorMiddleware(middleware: ErrorRequestHandler): void {
+    this.express.use(middleware);
+  }
+
+  public useJwtMiddleware(jwtMiddleware: RequestHandler) {
+    this.express.use(jwtMiddleware);
+  }
+
+  public start(PORT: number): void {
+    this.express.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  }
+}
+
+export default App;
